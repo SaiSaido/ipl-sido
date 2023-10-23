95bb8c6d7ad6481952292f0f37ace76abb0120a0
SaiSaido <sidoaria13@gmail.com>
2023-10-23 23:39:17 +0700

    ubah sesuai responsibility

---
+++ srp/Order.java
@@ -0,0 +1,20 @@
+package srp;
+
+class Order {
+    void calculateTotalSum() {
+    /*...*/
+    }
+    void getItems() {
+    /*...*/
+    }
+    void getItemCount() {
+    /*...*/
+    }
+    void addItem(Item item) {
+    /*...*/
+    }
+    void deleteItem(Item item) {
+    /*...*/
+    }
+
+}
